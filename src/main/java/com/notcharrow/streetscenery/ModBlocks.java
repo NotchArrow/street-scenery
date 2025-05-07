a4cd402cc93dfd3a4e0383ef986ba238c233d43b
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2025-05-06 20:48:42 -0400

    Basic Functionality

---
+++ src/main/java/com/notcharrow/streetscenery/ModBlocks.java
@@ -0,0 +1,37 @@
+package com.notcharrow.streetscenery;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+	public static final Block CONE_BLOCK = registerBlock("cone_block",
+			new Block(AbstractBlock.Settings.copy(Blocks.WHITE_CONCRETE)));
+
+	private static void addItemsToGroup(FabricItemGroupEntries entries) {
+		entries.add(CONE_BLOCK);
+	}
+
+	private static Block registerBlock(String name, Block block) {
+		registerBlockItem(name, block);
+		return Registry.register(Registries.BLOCK, Identifier.of(StreetScenery.MOD_ID, name), block);
+	}
+
+	private static Item registerBlockItem(String name, Block block) {
+		return Registry.register(Registries.ITEM, Identifier.of(StreetScenery.MOD_ID, name),
+				new BlockItem(block, new Item.Settings()));
+	}
+
+	public static void registerModBlocks() {
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(ModBlocks::addItemsToGroup);
+	}
+}
